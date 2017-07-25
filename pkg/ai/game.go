d6b5b9f91585149de586baee54fc8b0e6ce8de8d
Alex Pliutau <a.pliutau@gmail.com>
2017-07-25 21:17:50 +0700

    Add game state props

--- pkg/ai/game.go
+++ pkg/ai/game.go
@@ -1,10 +1,18 @@
 package ai
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 // Game struct
 type Game struct {
-	GreetingDone bool
+	GreetingDone    bool
+	UserWantsToPlay bool
+	Started         bool
+	Completed       bool
+	Canceled        bool
+	CreatedAt       time.Time
 }
 
 // GameManager struct
@@ -23,6 +31,7 @@ func NewGameManager() *GameManager {
 // InitSession func
 func (gm *GameManager) InitSession(sessionID string) {
 	gm.Games[sessionID] = new(Game)
+	gm.Games[sessionID].CreatedAt = time.Now()
 }
 
 // Reply func
@@ -32,19 +41,33 @@ func (gm *GameManager) Reply(sessionID string, userInput string) (string, error)
 		return "", errors.New("Sorry, game not found, please try to refresh a page.")
 	}
 
+	if game.Canceled {
+		return "", nil
+	}
+
 	userInput = sanitizeInput(userInput)
 
 	if !game.GreetingDone {
 		game.GreetingDone = true
 		return "Hi, my name is Alex. Do you want to play a game?", nil
 	}
-	if inputBelongsToIndent(userInput, noIntent) {
-		return "Negative answer!", nil
-		//return "Got it. See you later, alligator.", nil
-	}
+
 	if inputBelongsToIndent(userInput, yesIntent) {
-		return "Positive answer!", nil
-		//return "Today we have a game called '20 questions'. Let's play?", nil
+		if !game.UserWantsToPlay {
+			game.UserWantsToPlay = true
+			return "I have a game called '20 questions'. Let's play?", nil
+		}
+		if game.UserWantsToPlay && !game.Started {
+			game.Started = true
+			return "It's a simple fun game where you should guess a noun by asking a maximum of 20 questions. Question can be answered only with Yes, No and Don't know. Try to ask something!", nil
+		}
+	}
+
+	if inputBelongsToIndent(userInput, noIntent) {
+		if !game.UserWantsToPlay {
+			game.Canceled = true
+			return "Got it! See you later, alligator!", nil
+		}
 	}
 
 	return "Sorry, what do you mean?", nil
